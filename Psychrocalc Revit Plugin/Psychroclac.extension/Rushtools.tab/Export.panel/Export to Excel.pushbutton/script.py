28d6fe831cbec4aa4a6c3488f5d67ff7e3a4dbd1
Rushikesh Patil <coolrushipatil1995@gmail.com>
2023-11-17 15:50:03 -0800

    tested

--- Psychrocalc Revit Plugin/Psychroclac.extension/Rushtools.tab/Export.panel/Export to Excel.pushbutton/script.py
+++ Psychrocalc Revit Plugin/Psychroclac.extension/Rushtools.tab/Export.panel/Export to Excel.pushbutton/script.py
@@ -2,6 +2,7 @@
 import clr
 clr.AddReference('RevitAPI')
 from Autodesk.Revit.DB import FilteredElementCollector, BuiltInCategory, BuiltInParameter
+import csv
 
 # Access the active document in Revit
 doc = __revit__.ActiveUIDocument.Document
@@ -9,10 +10,16 @@ doc = __revit__.ActiveUIDocument.Document
 # Get all rooms in the project
 rooms = FilteredElementCollector(doc).OfCategory(BuiltInCategory.OST_Rooms).WhereElementIsNotElementType()
 
-# Loop through each room and print area, name, and number
-for room in rooms:
-    area = room.get_Parameter(BuiltInParameter.ROOM_AREA).AsDouble()
-    name = room.get_Parameter(BuiltInParameter.ROOM_NAME).AsString()
-    number = room.get_Parameter(BuiltInParameter.ROOM_NUMBER).AsString()
-    
-    print("Room Name: {}, Room Number: {}, Area: {:.2f} sq.ft.".format(name, number, area))
+# Define the file path
+file_path = "C:\\Users\\rush\\OneDrive - THERMA CORPORATION\\Desktop\\room_data.csv"
+
+with open(file_path, mode='w') as file:
+    writer = csv.writer(file)
+    writer.writerow(["Room Name", "Room Number", "Area (sq.ft.)"])  # Write header row
+    for room in rooms:
+        area = room.get_Parameter(BuiltInParameter.ROOM_AREA).AsDouble()
+        name = room.get_Parameter(BuiltInParameter.ROOM_NAME).AsString()
+        number = room.get_Parameter(BuiltInParameter.ROOM_NUMBER).AsString()
+        writer.writerow([name, number, round(area, 2)])
+
+    print("Data exported to " + file_path)
